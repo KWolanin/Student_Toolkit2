e0dc0b113287f12f196f9152a101428cd821caf8
wolanin <wolanin4u@gmail.com>
2019-08-28 18:50:20 +0200

    PolishLang.properties integrated (95%)

---
+++ src/main/java/wolanin/studentToolkit/frame/FrameLayout.java
@@ -0,0 +1,42 @@
+package wolanin.studentToolkit.frame;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static wolanin.studentToolkit.MainFrame.tabPanel;
+import static wolanin.studentToolkit.MainFrame.tabs;
+
+public class FrameLayout {
+
+	public static void setGroupLayout(Container contentPane) {
+		GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+		contentPane.setLayout(contentPaneLayout);
+		contentPaneLayout.setHorizontalGroup(
+				contentPaneLayout.createParallelGroup()
+						.addGroup(contentPaneLayout.createSequentialGroup()
+								.addContainerGap()
+								.addComponent(tabPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addGap(5, 5, 5))
+		);
+		contentPaneLayout.setVerticalGroup(
+				contentPaneLayout.createParallelGroup()
+						.addGroup(GroupLayout.Alignment.TRAILING, contentPaneLayout.createSequentialGroup()
+								.addComponent(tabPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addContainerGap())
+		);
+		//frame.setLocationRelativeTo(getOwner());
+		GroupLayout tabPanelLayout = new GroupLayout(tabPanel);
+		tabPanel.setLayout(tabPanelLayout);
+		tabPanelLayout.setHorizontalGroup(
+				tabPanelLayout.createParallelGroup()
+						.addComponent(tabs, GroupLayout.Alignment.TRAILING)
+		);
+		tabPanelLayout.setVerticalGroup(
+				tabPanelLayout.createParallelGroup()
+						.addGroup(tabPanelLayout.createSequentialGroup()
+								.addContainerGap()
+								.addComponent(tabs, GroupLayout.DEFAULT_SIZE, 0, Short.MAX_VALUE))
+		);
+	}
+
+}
---
+++ src/main/java/wolanin/studentToolkit/language/LangProperties.java
@@ -0,0 +1,23 @@
+package wolanin.studentToolkit.language;
+
+import wolanin.studentToolkit.MainFrame;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class LangProperties {
+
+	public static Properties setProperties() throws IOException {
+		FileInputStream fileInputStream;
+		if (MainFrame.PLlang) {
+			fileInputStream = new FileInputStream("polishLang.properties");
+		} else {
+			fileInputStream = new FileInputStream("englishLang.properties");
+		}
+		Properties properties = new Properties();
+		properties.load(fileInputStream);
+
+		return properties;
+	}
+}
